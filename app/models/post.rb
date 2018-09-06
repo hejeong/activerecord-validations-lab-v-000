73a7f7dac3fb0212831be99e0f3b3ae5f452dd1f
Jonathan Hong <jonhong777@gmail.com>
2018-09-06 04:28:48 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,12 @@
-class ClickbaitValidator < ActiveModel::EachValidator
-  def clickbait?(record, attribute, value)
-    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
-      record.errors[attribute] << (options[:message] || "is a clickbait-y title.")
-    end
-  end
-end
-
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity?
+
+  def clickbaity?
+    clickbait_words = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+    if :title.match()
+  end
 end
