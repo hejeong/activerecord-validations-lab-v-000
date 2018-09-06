f7ff07998f0b4c6f3e0ad52e9138c8526a4f1c62
Jonathan Hong <jonhong777@gmail.com>
2018-09-06 04:13:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
+class ClickbaitValidator < ActiveModel::EachValidator 
+
+end
+
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validate
+  validate :title, 
 end
